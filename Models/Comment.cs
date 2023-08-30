df8554441198631903796b87e2d74070653245df
opilane <opilane@HP-CZC2349HT0>
2023-08-30 15:59:16 +0300

    Add project files.

---
+++ Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace veeb.Models
+{
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Content { get; set; }
+        public int ArticleId { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
