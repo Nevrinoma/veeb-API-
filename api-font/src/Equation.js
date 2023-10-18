58a65b6aa6ab3d6966370d19ea52da5c8d6ffd45
opilane <opilane@HP-CZC2349HV2>
2023-10-18 14:57:50 +0300

    s

--- api-font/src/Equation.js
+++ api-font/src/Equation.js
@@ -1,24 +1,33 @@
+import React, { useState, useEffect } from 'react';
 
-import React, {useState, useEffect} from 'react';
+function Equation({ equation }) {
+  const [result, setResult] = useState(null);
 
-function Equation({ equation, operator}) {
-    const [loogika, setLoogika] = useState([]);
+  useEffect(() => {
+    
+    const operator = equation.split(' ')[1]; 
+    const operands = equation.split(' '); 
 
-    function AND_operator({ operandA, operandB }){
-        useEffect(() => {
-            fetch("https://localhost:7118/api/ANDloogika/and_operatsioon/" + {operandA} + "/" + {operandB})
-              .then(res => res.json())
-              .then(json => setLoogika(json));
-            }, []);
+    if (operator === 'and' || operator === 'or') {
+      const [operandA, , operandB] = operands;
+      fetch(`https://localhost:7118/api/ANDloogika/${operator}_operatsioon/${operandA}/${operandB}`)
+        .then((res) => res.json())
+        .then((json) => setResult(json));
+    } else if (operator === 'not') {
+      const [operandA] = operands;
+      fetch(`https://localhost:7118/api/ANDloogika/${operator}_operatsioon/${operandA}`)
+        .then((res) => res.json())
+        .then((json) => setResult(json));
     }
+  }, [equation]);
 
-    return (
-        <div className="equation">
-            <div className="equation-text">
-                <label>{equation} {operator} = {loogika}</label>
-            </div>
-        </div>
-    );
+  return (
+    <div className="equation">
+      <div className="equation-text">
+        <label>{equation} = {result !== null ? result : 'Calculating...'}</label>
+      </div>
+    </div>
+  );
 }
 
 export default Equation;
